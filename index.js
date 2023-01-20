13629bd310921220cd197e8467cd15b0cb9f53c6
Heath Stewart <heaths@outlook.com>
2023-01-19 19:36:59 -0800

    Remove unnecessary code

--- index.js
+++ index.js
@@ -16,41 +16,63 @@ packageFile.onchange = () => {
         if (e.target.readyState === FileReader.DONE) {
             const arrayBuffer = e.target.result;
             const array = new Uint8Array(arrayBuffer);
-            const productInfo = getProductInfo(array);
-            info.innerHTML = `<b>ProductName:</b> <code>${productInfo.name}</code><br><b>ProductVersion:</b> <code>${productInfo.version}</code>`;
-            info.style = "display: block";
-
-            const pkg = new Package(array);
-            const _tables = pkg.tables();
-            let names = "<ul>";
-            for (let i in _tables) {
-                const _table = _tables[i];
-                const _columns = _table.columns().map((c) => {
-                    if (c.primaryKey) {
-                       return `<b>${c.name}</b>`;
-                    }
-
-                    if (c.nullable) {
-                        return `${c.name}?`;
-                    }
-
-                    return c.name
-                });
-                names += `<li>${_table.name} (${_columns.join(", ")})</li>`
+            try {
+                const pkg = new Package(array);
+                render(pkg);
+            } catch (e) {
+                window.alert(`Failed to load the package: ${e}`);
             }
-            names += "</ul>";
-            tables.innerHTML = "<p><b>Tables</b></p>" + names;
-            tables.style = "display: block";
-
-            const _props = pkg.rows("Property").sort((a, b) => a['Property'] > b['Property'] ? 1 : -1);
-            let _properties = "<table><tr><th>Property</th><th>Value</th></tr>";
-            for (let i in _props) {
-                const prop = _props[i];
-                _properties += `<tr><td>${prop['Property']}</td><td>${prop['Value']}</td></tr>`;
-            }
-            _properties += "</table>";
-            rows.innerHTML = "<p><b>Properties</b></p>" + _properties;
-            rows.style = "display: block";
         }
+    }
+}
+
+function render(pkg) {
+    const productInfo = pkg.productInfo();
+    if (productInfo) {
+        info.innerHTML = `<b>ProductName:</b> <code>${productInfo.name}</code><br>` +
+                        `<b>ProductVersion:</b> <code>${productInfo.version}</code><br>` +
+                        `<b>Manufacturer:</b> <code>${productInfo.manufacturer}</code>`;
+        if (productInfo.upgradeCode) {
+            info.innerHTML += `<br><b>UpgradeCode:</b> <code>${productInfo.upgradeCode}</code>`;
+        }
+        info.style = "display: block";
+    } else {
+        info.style = "display: none";
+    }
+
+    const _tables = pkg.tables();
+    let names = "<ul>";
+    for (let i in _tables) {
+        const _table = _tables[i];
+        const _columns = _table.columns().map((c) => {
+            if (c.primaryKey) {
+               return `<b>${c.name}</b>`;
+            }
+
+            if (c.nullable) {
+                return `${c.name}?`;
+            }
+
+            return c.name
+        });
+        names += `<li>${_table.name} (${_columns.join(", ")})</li>`
+    }
+    names += "</ul>";
+    tables.innerHTML = "<p><b>Tables</b></p>" + names;
+    tables.style = "display: block";
+
+    try {
+        const _props = pkg.rows("Property").sort((a, b) => a['Property'] > b['Property'] ? 1 : -1);
+        let _properties = "<table><tr><th>Property</th><th>Value</th></tr>";
+        for (let i in _props) {
+            const prop = _props[i];
+            _properties += `<tr><td>${prop['Property']}</td><td>${prop['Value']}</td></tr>`;
+        }
+        _properties += "</table>";
+        rows.innerHTML = "<p><b>Properties</b></p>" + _properties;
+        rows.style = "display: block";
+    } catch {
+        console.log("'Property' table not found");
+        rows.style = "display: none";
     }
 }
